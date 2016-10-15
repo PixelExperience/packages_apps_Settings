55fa85695977eb66a58432c0ae5dfc377c434f8d
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-10-15 19:02:17 +0200

    DisplaySettings: font size in 5% steps from 80% to 130%
    Change-Id: I80e490e5e435e39e40f102066eeeeb712e8ba01e

--- src/com/android/settings/accessibility/AccessibilitySettings.java
+++ src/com/android/settings/accessibility/AccessibilitySettings.java
@@ -671,8 +671,8 @@ public class AccessibilitySettings extends SettingsPreferenceFragment implements
         final float currentScale = Settings.System.getFloat(getContext().getContentResolver(),
                 Settings.System.FONT_SCALE, 1.0f);
         final Resources res = getContext().getResources();
-        final String[] entries = res.getStringArray(R.array.entries_font_size);
-        final String[] strEntryValues = res.getStringArray(R.array.entryvalues_font_size);
+        final String[] entries = res.getStringArray(R.array.entries_font_size_percent_custom);
+        final String[] strEntryValues = res.getStringArray(R.array.entryvalues_font_size_custom);
         final int index = ToggleFontSizePreferenceFragment.fontSizeValueToIndex(currentScale,
                 strEntryValues);
         pref.setSummary(entries[index]);
