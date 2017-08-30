3c07d61e7715d2aff07f068fc0e9984508c6205f
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-08-30 20:16:04 -0300

    StorageSettings: Use pixel color in storage bar / icon
    Change-Id: Id56ea292117df7abdb7a53cff76bc0328977f4f1

--- src/com/android/settings/deviceinfo/StorageSettings.java
+++ src/com/android/settings/deviceinfo/StorageSettings.java
@@ -77,7 +77,7 @@ public class StorageSettings extends SettingsPreferenceFragment implements Index
     static final int COLOR_PUBLIC = Color.parseColor("#ff9e9e9e");
 
     static final int[] COLOR_PRIVATE = new int[]{
-            Color.parseColor("#ff26a69a"),
+            Color.parseColor("#ff1a73e8"),
             Color.parseColor("#ffab47bc"),
             Color.parseColor("#fff2a600"),
             Color.parseColor("#ffec407a"),
