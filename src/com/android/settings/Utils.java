01dc05320e8e97fe69d563ade2fd83969e83141f
Bruno Martins <bgcngm@gmail.com>
2017-10-25 11:43:13 +0100

    ButtonSettings: Add a RemotePreference for device-specific panel
     * In case a device provides its own panel for extra buttons
       settings, place it at the bottom of the Button Settings screen.
       Back in N, that used to go directly to the Settings dashboard,
       but no longer makes sense in O.
    
     * This uses the RemotePreference API to allow the external app
       to update the view and the action for this is:
    
         "org.lineageos.settings.device.ADDITIONAL_BUTTONS_SETTINGS"
    
    Change-Id: If071eaad8061838a7cddea4fe1df3308f2f9ec84

--- src/com/android/settings/Utils.java
+++ src/com/android/settings/Utils.java
@@ -1225,4 +1225,28 @@ public final class Utils extends com.android.settingslib.Utils {
     public static boolean isProviderModelEnabled(Context context) {
         return FeatureFlagUtils.isEnabled(context, FeatureFlagUtils.SETTINGS_PROVIDER_MODEL);
     }
+
+    /**
+     * Checks if a package is available to handle the given action.
+     */
+    public static boolean canResolveIntent(Context context, Intent intent) {
+        // check whether the target handler exist in system
+        PackageManager pm = context.getPackageManager();
+        List<ResolveInfo> results = pm.queryIntentActivitiesAsUser(intent,
+                PackageManager.MATCH_SYSTEM_ONLY,
+                UserHandle.myUserId());
+        for (ResolveInfo resolveInfo : results) {
+            // check is it installed in system.img, exclude the application
+            // installed by user
+            if ((resolveInfo.activityInfo.applicationInfo.flags &
+                    ApplicationInfo.FLAG_SYSTEM) != 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean canResolveIntent(Context context, String action) {
+        return canResolveIntent(context, new Intent(action));
+    }
 }
