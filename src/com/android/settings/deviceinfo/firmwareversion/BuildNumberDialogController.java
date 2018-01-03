41884dc875d9aa5952e86b29072e3703d64e91d7
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-01-03 03:38:03 +0000

    Settings: Append PixelExperience build date/type on build display id
    Change-Id: I831800ae0a769513b6b0c008ba492cd8334b3b65

--- src/com/android/settings/deviceinfo/firmwareversion/BuildNumberDialogController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/BuildNumberDialogController.java
@@ -22,6 +22,8 @@ import android.text.BidiFormatter;
 
 import com.android.settings.R;
 
+import android.os.SystemProperties;
+
 public class BuildNumberDialogController {
 
     @VisibleForTesting
@@ -33,11 +35,24 @@ public class BuildNumberDialogController {
         mDialog = dialog;
     }
 
+    private String getPixelExperienceVersion(){
+        String buildDate = SystemProperties.get("org.pixelexperience.build_date","");
+        String buildType = SystemProperties.get("org.pixelexperience.build_type","unofficial").toUpperCase();
+        return buildDate.equals("") ? "" : "PixelExperience-" + buildDate + "-" + buildType;
+    }
+
     /**
      * Updates the build number to the dialog.
      */
     public void initialize() {
-        mDialog.setText(BUILD_NUMBER_VALUE_ID,
-                BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY));
+        
+        StringBuilder sb = new StringBuilder();
+        sb.append(BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY));
+        String pixelExperienceVersion = getPixelExperienceVersion();
+        if (!pixelExperienceVersion.equals("")){
+            sb.append("\n");
+            sb.append(pixelExperienceVersion);
+        }
+        mDialog.setText(BUILD_NUMBER_VALUE_ID, sb.toString());
     }
 }
