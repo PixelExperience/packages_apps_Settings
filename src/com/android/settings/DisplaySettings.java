b7e5c04fc66969f4b349d69e3aa8711e7352e10a
TheRingMaster <gzroms@hotmail.com>
2018-03-08 23:35:29 +0530

    Settings: Add toggle for proximity wake
    Change-Id: I33d492e9ef49ba3ee4b43b5dc8180f782bd6d801
    
    PixelExperience edits: Also check config_proximityCheckOnWakeEnabledByDefault
    
    Signed-off-by: Henrique Silva <jhenrique09.mcz@hotmail.com>

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -32,6 +32,7 @@ import com.android.settings.display.FontSizePreferenceController;
 import com.android.settings.display.LiftToWakePreferenceController;
 import com.android.settings.display.NightDisplayPreferenceController;
 import com.android.settings.display.NightModePreferenceController;
+import com.android.settings.display.ProximityOnWakePreferenceController;
 import com.android.settings.display.ScreenSaverPreferenceController;
 import com.android.settings.display.TapToWakePreferenceController;
 import com.android.settings.display.ThemePreferenceController;
@@ -96,6 +97,7 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new LiftToWakePreferenceController(context));
         controllers.add(new NightDisplayPreferenceController(context));
         controllers.add(new NightModePreferenceController(context));
+        controllers.add(new ProximityOnWakePreferenceController(context));        
         controllers.add(new ScreenSaverPreferenceController(context));
         controllers.add(new AmbientDisplayPreferenceController(
                 context,
