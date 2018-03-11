d62779aa07d9b4a37b9588472ca80e59c6037136
LuK1337 <priv.luk@gmail.com>
2018-03-11 11:38:52 +0100

    Settings: Add high touch sensitivity and touchscreen hovering toggles
    squashed with: I98ce66dec9801c36da5a2facd6b033895b7963f0
                   DisplaySettings: Hide high touch sensitivity from search if needed
    
    Change-Id: Ib8712d2d0c0fe4396dbea7c7fc130e0c253d3207

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -42,6 +42,8 @@ import com.android.settingslib.search.SearchIndexable;
 
 import com.android.settings.development.RefreshRatePreferenceController;
 
+import com.android.internal.custom.hardware.LineageHardwareManager;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -50,6 +52,7 @@ public class DisplaySettings extends DashboardFragment {
     private static final String TAG = "DisplaySettings";
 
     private static final String KEY_SCREEN_TIMEOUT = "screen_timeout";
+    private static final String KEY_HIGH_TOUCH_SENSITIVITY = "high_touch_sensitivity_enable";
 
     @Override
     public int getMetricsCategory() {
@@ -111,6 +114,17 @@ public class DisplaySettings extends DashboardFragment {
                     sir.xmlResId = R.xml.display_settings;
                     result.add(sir);
                     return result;
+                }
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    List<String> keys = super.getNonIndexableKeys(context);
+                    LineageHardwareManager hardware = LineageHardwareManager.getInstance(context);
+                    if (!hardware.isSupported(
+                            LineageHardwareManager.FEATURE_HIGH_TOUCH_SENSITIVITY)) {
+                        keys.add(KEY_HIGH_TOUCH_SENSITIVITY);
+                    }
+                    return keys;
                 }
 
                 @Override
