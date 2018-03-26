ae587e5eec9b7d29cfe38e5b58dd0853da649bcf
Shubham Singh <coolsks94@gmail.com>
2018-03-27 03:49:27 +0530

    Make aosp theme selector unavailable
     Since we have custom AccentPicker (Fruity pebbles) from DU
    
    Change-Id: I8d3b07f3a013966f835fad10362c4f402589afb6
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- src/com/android/settings/display/ThemePreferenceController.java
+++ src/com/android/settings/display/ThemePreferenceController.java
@@ -149,9 +149,10 @@ public class ThemePreferenceController extends AbstractPreferenceController impl
 
     @Override
     public boolean isAvailable() {
-        if (mOverlayService == null) return false;
-        String[] themes = getAvailableThemes();
-        return themes != null && themes.length > 1;
+        //if (mOverlayService == null) return false;
+        //String[] themes = getAvailableThemes();
+        //return themes != null && themes.length > 1;
+        return false;
     }
 
 
