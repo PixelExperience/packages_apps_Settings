ac5045fc4e2a2b6c962c4f661faafa1e6e6ae7d4
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-06-01 20:21:05 -0300

    Settings: [1/3] Notification theme switch
    Change-Id: Ic3a5f8f04549df3ef833b5899b5330f6117f7300
    Signed-off-by: Henrique Silva <jhenrique09.mcz@hotmail.com>

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -31,6 +31,7 @@ import com.android.settings.display.BrightnessLevelPreferenceController;
 import com.android.settings.display.CameraGesturePreferenceController;
 import com.android.settings.display.ColorModePreferenceController;
 import com.android.settings.display.DarkUIPreferenceController;
+import com.android.settings.display.NotificationThemePreferenceController;
 import com.android.settings.display.FontSizePreferenceController;
 import com.android.settings.display.LiftToWakePreferenceController;
 import com.android.settings.display.NightDisplayPreferenceController;
@@ -98,6 +99,7 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new AutoRotatePreferenceController(context, lifecycle));
         controllers.add(new CameraGesturePreferenceController(context));
         controllers.add(new DarkUIPreferenceController(context));
+        controllers.add(new NotificationThemePreferenceController(context));
         controllers.add(new FontSizePreferenceController(context));
         controllers.add(new LiftToWakePreferenceController(context));
         controllers.add(new NightDisplayPreferenceController(context));
