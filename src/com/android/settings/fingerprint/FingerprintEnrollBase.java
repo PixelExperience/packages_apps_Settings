DirtyUnicorns/android_packages_apps_Settings

Fingerprint enroll automatic navbar toggle: fixes
don't remove navbar if no bottom fingerprint

Change-Id: I4914ed1f86dcde91d010ffa58444fb4991aaae6b

src/com/android/settings/fingerprint/FingerprintEnrollBase.java
@@ -72,7 +72,7 @@ public abstract class FingerprintEnrollBase extends InstrumentedActivity
     @Override
     protected void onStart() {
         super.onStart();
-        if (mNavBarEnabled) {
+        if (mHasBottomSensor && mNavBarEnabled) {
             mWasUsingNavbar = true;
             disableNavbar(true);
         }

PixelExperience/packages_apps_Settings

src/com/android/settings/fingerprint/FingerprintEnrollBase.java
@@ -71,7 +71,7 @@ public abstract class FingerprintEnrollBase extends InstrumentedActivity
     @Override
     protected void onStart() {
         super.onStart();
-        if (mNavBarEnabled) {
+        if (mHasFrontSensor && mNavBarEnabled) {
             mWasUsingNavbar = true;
             NavbarUtils.setNavigationBarEnabled(this, false);
         }