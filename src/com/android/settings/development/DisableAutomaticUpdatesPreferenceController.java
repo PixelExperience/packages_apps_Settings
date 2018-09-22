e7d7f53eb87c67c43884baad16b226387f0215ad
ezio84 <brabus84@gmail.com>
2018-09-22 01:31:50 -0500

    Remove the Automatic ota check option but be sure it's disabled
    Change-Id: I95e2d115a40904531019653129b284a1304a8043

--- src/com/android/settings/development/DisableAutomaticUpdatesPreferenceController.java
+++ src/com/android/settings/development/DisableAutomaticUpdatesPreferenceController.java
@@ -72,4 +72,9 @@ public class DisableAutomaticUpdatesPreferenceController extends
                 Settings.Global.OTA_DISABLE_AUTOMATIC_UPDATE, DISABLE_UPDATES_SETTING);
         ((SwitchPreference) mPreference).setChecked(false);
     }
+
+    @Override
+    public boolean isAvailable() {
+        return false;
+    }
 }
