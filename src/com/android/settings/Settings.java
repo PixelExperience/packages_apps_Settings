a5920b9aaee3ff2680c045b9008a7e5be15b558f
Chris Crump <chris@cypheros.co>
2018-10-13 16:25:51 +0530

    Add preference for Ambient Play
    - Updated for Pie by @spezi77
    
    @jhenrique09 edits:
    
    * Cleanup code
    * Add notification toggle
    
    Change-Id: I49a5d4b066cbe51b2be3c0808de6d880fa2f6fa0

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -166,4 +166,7 @@ public class Settings extends SettingsActivity {
     public static class AccountDashboardActivity extends SettingsActivity {}
     public static class SystemDashboardActivity extends SettingsActivity {}
 
+    // Ambient play
+    public static class AmbientPlayActivity extends SettingsActivity { /* empty */ }
+
 }
