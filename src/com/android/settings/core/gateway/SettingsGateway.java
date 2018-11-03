71317c5380904ce3831db90b4a1fb0024219bb33
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-11-03 15:34:29 -0300

    AmbientPlay: Introduce history manager [4/4]
    Change-Id: I5122596eea6e9e813342d85e571bcd472718f11e

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -136,6 +136,7 @@ import com.android.settings.wifi.WifiInfo;
 import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.calling.WifiCallingSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
+import com.android.settings.custom.ambient.history.AmbientPlayHistory;
 
 public class SettingsGateway {
 
@@ -261,6 +262,7 @@ public class SettingsGateway {
             DirectoryAccessDetails.class.getName(),
             ToggleBackupSettingFragment.class.getName(),
             PreviouslyConnectedDeviceDashboardFragment.class.getName(),
+            AmbientPlayHistory.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
