01d18fe0440be93f72f01e92fe049da2e342a4b3
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-12-20 10:19:22 -0200

    Remove now playing for now [2/4]
    Revert "AmbientPlay: Introduce history manager [4/4]"
    
    This reverts commit 1f754eecefa195dc416a70465ed72cf775ef05eb.
    
    Change-Id: Idc026c3c3971490d3db6871927b4816869a2c729
    
    Revert "Add preference for Ambient Play"
    
    This reverts commit d9f25fa6c73214e1d60014597a57e306f4b14143.
    
    Change-Id: I34492751c9ef35ac7f2bc0e2c4bd9febc5b26205

--- Android.mk
+++ Android.mk
@@ -21,7 +21,6 @@ LOCAL_USE_AAPT2 := true
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
-    $(ANDROID_SUPPORT_DESIGN_TARGETS) \
     android-slices-builders \
     android-slices-core \
     android-slices-view \
