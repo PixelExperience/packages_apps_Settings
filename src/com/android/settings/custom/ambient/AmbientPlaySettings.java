b55d484f810eff0d09fcec08a904a0ec6114dcbe
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-12-25 21:23:45 -0200

    Ambient Play: Introduce saving options [2/2]
    Change-Id: Ibfd3f523cd0ea3cdbe69d2fc75e2575161097222

--- src/com/android/settings/custom/ambient/AmbientPlaySettings.java
+++ src/com/android/settings/custom/ambient/AmbientPlaySettings.java
@@ -22,6 +22,7 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.provider.Settings;
+import android.support.v7.preference.Preference;
 import android.support.v14.preference.SwitchPreference;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -46,10 +47,12 @@ public class AmbientPlaySettings extends SettingsPreferenceFragment implements C
     private SwitchPreference mAmbientRecognitionKeyguardPreference;
     private SwitchPreference mAmbientRecognitionNotificationPreference;
     private AmbientPlayHistoryPreference mAmbientRecognitionHistoryPreference;
+    private Preference mAmbientRecognitionSavingOptionsPreference;
 
     private String AMBIENT_RECOGNITION_KEYGUARD = "ambient_recognition_keyguard";
     private String AMBIENT_RECOGNITION_NOTIFICATION = "ambient_recognition_notification";
     private String AMBIENT_RECOGNITION_HISTORY = "ambient_recognition_history_preference";
+    private String AMBIENT_RECOGNITION_SAVING_OPTIONS = "ambient_recognition_saving_options";
 
     private BroadcastReceiver onSongMatch = new BroadcastReceiver() {
         @Override
@@ -74,6 +77,8 @@ public class AmbientPlaySettings extends SettingsPreferenceFragment implements C
         mAmbientRecognitionNotificationPreference = (SwitchPreference) findPreference(AMBIENT_RECOGNITION_NOTIFICATION);
         mAmbientRecognitionNotificationPreference.setEnabled(isEnabled());
         mAmbientRecognitionHistoryPreference = (AmbientPlayHistoryPreference) findPreference(AMBIENT_RECOGNITION_HISTORY);
+        mAmbientRecognitionSavingOptionsPreference = (Preference) findPreference(AMBIENT_RECOGNITION_SAVING_OPTIONS);
+        mAmbientRecognitionSavingOptionsPreference.setEnabled(isEnabled());
     }
 
     @Override
@@ -132,5 +137,6 @@ public class AmbientPlaySettings extends SettingsPreferenceFragment implements C
         mTextView.setText(getString(isChecked ? R.string.ambient_play_switch_bar_on : R.string.ambient_play_switch_bar_off));
         mAmbientRecognitionKeyguardPreference.setEnabled(isChecked);
         mAmbientRecognitionNotificationPreference.setEnabled(isChecked);
+        mAmbientRecognitionSavingOptionsPreference.setEnabled(isChecked);
     }
 }
