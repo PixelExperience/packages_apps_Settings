05621ebee4a7318c4693d2d7577e41df9bf07927
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-03-13 15:07:55 -0300

    Revert "Invoke vendor MobileNetworkSettings from data usage"
    This reverts commit a0bbaae466dc7ccba9e1773d7480f81becd73b45.

--- src/com/android/settings/datausage/DataUsageSummaryLegacy.java
+++ src/com/android/settings/datausage/DataUsageSummaryLegacy.java
@@ -34,7 +34,6 @@ import android.text.SpannableString;
 import android.text.TextUtils;
 import android.text.format.Formatter;
 import android.text.style.RelativeSizeSpan;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -160,16 +159,8 @@ public class DataUsageSummaryLegacy extends DataUsageBaseFragment implements Ind
         switch (item.getItemId()) {
             case R.id.data_usage_menu_cellular_networks: {
                 final Intent intent = new Intent(Intent.ACTION_MAIN);
-                if (Utils.isNetworkSettingsApkAvailable()) {
-                    // prepare intent to start qti MobileNetworkSettings activity
-                    intent.setComponent(new ComponentName("com.qualcomm.qti.networksetting",
-                            "com.qualcomm.qti.networksetting.MobileNetworkSettings"));
-                } else {
-                    // vendor MobileNetworkSettings not available, launch the default activity
-                    Log.d(TAG, "vendor MobileNetworkSettings is not available");
-                    intent.setComponent(new ComponentName("com.android.phone",
-                            "com.android.phone.MobileNetworkSettings"));
-                }
+                intent.setComponent(new ComponentName("com.android.phone",
+                        "com.android.phone.MobileNetworkSettings"));
                 startActivity(intent);
                 return true;
             }
