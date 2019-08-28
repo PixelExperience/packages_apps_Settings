c869ec416e93f6554b8227fb45101b20c2397b35
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-08-28 13:43:46 -0300

    Settings: Dark theme based on time [2/2]
    Change-Id: Ie13f53c01773e33e82699e1fe066cec40434d6d3

--- src/com/android/settings/display/SystemUiThemePreferenceController.java
+++ src/com/android/settings/display/SystemUiThemePreferenceController.java
@@ -18,6 +18,7 @@ package com.android.settings.display;
 
 import static android.provider.Settings.Secure.THEME_MODE;
 import static android.provider.Settings.Secure.THEME_MODE_LIGHT;
+import static android.provider.Settings.Secure.THEME_MODE_TIME;
 
 import android.content.Context;
 import android.provider.Settings;
@@ -39,7 +40,9 @@ public class SystemUiThemePreferenceController extends BasePreferenceController
 
     private ListPreference mSystemUiThemePref;
     private ListPreference mDarkThemePref;
+    private Preference mThemeTimePref;
     private String KEY_DARK_THEME_STYLE = "dark_theme_style";
+    private String KEY_THEME_TIME_SETTINGS = "theme_time_settings";
 
     public SystemUiThemePreferenceController(Context context, String preferenceKey) {
         super(context, preferenceKey);
@@ -56,9 +59,11 @@ public class SystemUiThemePreferenceController extends BasePreferenceController
         super.displayPreference(screen);
         mSystemUiThemePref = (ListPreference) screen.findPreference(getPreferenceKey());
         mDarkThemePref = (ListPreference) screen.findPreference(KEY_DARK_THEME_STYLE);
+        mThemeTimePref = (Preference) screen.findPreference(KEY_THEME_TIME_SETTINGS);
         int value = Settings.Secure.getInt(mContext.getContentResolver(), THEME_MODE, 0);
         mSystemUiThemePref.setValue(Integer.toString(value));
         mDarkThemePref.setEnabled(value != THEME_MODE_LIGHT);
+        mThemeTimePref.setEnabled(value == THEME_MODE_TIME);
     }
 
     @Override
@@ -67,6 +72,7 @@ public class SystemUiThemePreferenceController extends BasePreferenceController
         Settings.Secure.putInt(mContext.getContentResolver(), THEME_MODE, value);
         refreshSummary(preference);
         mDarkThemePref.setEnabled(value != THEME_MODE_LIGHT);
+        mThemeTimePref.setEnabled(value == THEME_MODE_TIME);
         return true;
     }
 
