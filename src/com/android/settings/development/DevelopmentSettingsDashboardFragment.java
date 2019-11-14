967f4ae7f484c7531d3737f82e431297a1261d76
Anushek Prasal <anushekprasal@gmail.com>
2019-11-14 18:38:46 +0530

    Settings: Add refresh rate setting [2/2]
    Change-Id: I6b7fd0d115ac195df20381d7103d4419b2672b3f

--- src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
+++ src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
@@ -44,6 +44,7 @@ import com.android.settings.Utils;
 import com.android.settings.dashboard.RestrictedDashboardFragment;
 import com.android.settings.development.autofill.AutofillLoggingLevelPreferenceController;
 import com.android.settings.development.autofill.AutofillResetOptionsPreferenceController;
+import com.android.settings.development.RefreshRatePreferenceController;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
 import com.android.settings.widget.SwitchBar;
@@ -496,6 +497,7 @@ public class DevelopmentSettingsDashboardFragment extends RestrictedDashboardFra
         controllers.add(new AutofillResetOptionsPreferenceController(context));
         controllers.add(new TrustAgentsExtendUnlockPreferenceController(context));
         controllers.add(new TrustLostLocksScreenPreferenceController(context));
+        controllers.add(new RefreshRatePreferenceController(context));
         return controllers;
     }
 
