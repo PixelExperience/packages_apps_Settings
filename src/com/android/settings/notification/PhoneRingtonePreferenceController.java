0bd8d88285e323e4f34b066f65a76cb4cf09ce3f
Dyneteve <sound0020@gmail.com>
2019-12-12 17:24:23 +0000

    Settings: Squash revert ringtone commits
    This reverts commit ff11e9f5fd21cc9dab86593c24a0f39a9c0ae3ed and 45969af675e56a6149059dbee41d1944b4afea98
    
    Change-Id: I865db9f151cce0e1353e7aeae09001adb5e3dc46

--- src/com/android/settings/notification/PhoneRingtonePreferenceController.java
+++ src/com/android/settings/notification/PhoneRingtonePreferenceController.java
@@ -19,38 +19,14 @@ package com.android.settings.notification;
 import android.content.Context;
 import android.media.RingtoneManager;
 
-import android.telephony.TelephonyManager;
-
-import androidx.preference.PreferenceScreen;
-
-import com.android.settings.DefaultRingtonePreference;
-import com.android.settings.R;
-
 import com.android.settings.Utils;
 
 public class PhoneRingtonePreferenceController extends RingtonePreferenceControllerBase {
 
-    private static final int SLOT_ID = 0;
     private static final String KEY_PHONE_RINGTONE = "ringtone";
 
     public PhoneRingtonePreferenceController(Context context) {
         super(context);
-    }
-
-    @Override
-    public void displayPreference(PreferenceScreen screen) {
-        super.displayPreference(screen);
-
-        TelephonyManager telephonyManager =
-                (TelephonyManager) mContext.getSystemService(Context.TELEPHONY_SERVICE);
-        if (telephonyManager.isMultiSimEnabled()) {
-            // For Multi SIM device, shoud show "Phone ringtone - SIM 1" for slot1 ringtone setting.
-            DefaultRingtonePreference ringtonePreference =
-                    (DefaultRingtonePreference) screen.findPreference(KEY_PHONE_RINGTONE);
-            ringtonePreference.setTitle(mContext.getString(R.string.ringtone_title) + " - " +
-                String.format(mContext.getString(R.string.sim_card_number_title), 1));
-            ringtonePreference.setEnabled(hasCard());
-        }
     }
 
     @Override
@@ -66,11 +42,5 @@ public class PhoneRingtonePreferenceController extends RingtonePreferenceControl
     @Override
     public int getRingtoneType() {
         return RingtoneManager.TYPE_RINGTONE;
-    }
-
-    private boolean hasCard() {
-        TelephonyManager telephonyManager =
-                (TelephonyManager) mContext.getSystemService(Context.TELEPHONY_SERVICE);
-        return telephonyManager.hasIccCard(SLOT_ID);
     }
 }
