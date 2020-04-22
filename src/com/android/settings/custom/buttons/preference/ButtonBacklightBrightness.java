1a4b16cb0b2429aa6f51f5303bdfaf3c59b2d630
Bruno Martins <bgcngm@gmail.com>
2020-04-23 00:16:45 +0100

    DeviceUtils: Add helper methods to retrieve device's key capabilities
    Only one key related method existed so far, therefore add the remaining ones
    and use those.
    
    Change-Id: I586ad2f783087e6bd2606262fa1d067ed76df0a6

--- src/com/android/settings/custom/buttons/preference/ButtonBacklightBrightness.java
+++ src/com/android/settings/custom/buttons/preference/ButtonBacklightBrightness.java
@@ -40,8 +40,7 @@ import androidx.preference.PreferenceManager;
 
 import com.android.settings.R;
 
-import static com.android.internal.util.custom.hwkeys.DeviceKeysConstants.*;
-
+import com.android.settings.custom.utils.DeviceUtils;
 import com.android.settings.custom.preference.CustomDialogPreference;
 
 public class ButtonBacklightBrightness extends CustomDialogPreference<AlertDialog> implements
@@ -203,15 +202,14 @@ public class ButtonBacklightBrightness extends CustomDialogPreference<AlertDialo
     }
 
     public boolean isButtonSupported() {
-        final Resources res = getContext().getResources();
-        final int deviceKeys = res.getInteger(
-                com.android.internal.R.integer.config_deviceHardwareKeys);
+        final Context context = getContext();
+        final Resources res = context.getResources();
         // All hardware keys besides volume and camera can possibly have a backlight
-        boolean hasBacklightKey = (deviceKeys & KEY_MASK_HOME) != 0
-                || (deviceKeys & KEY_MASK_BACK) != 0
-                || (deviceKeys & KEY_MASK_MENU) != 0
-                || (deviceKeys & KEY_MASK_ASSIST) != 0
-                || (deviceKeys & KEY_MASK_APP_SWITCH) != 0;
+        boolean hasBacklightKey = DeviceUtils.hasHomeKey(context)
+                || DeviceUtils.hasBackKey(context)
+                || DeviceUtils.hasMenuKey(context)
+                || DeviceUtils.hasAssistKey(context)
+                || DeviceUtils.hasAppSwitchKey(context);
         boolean hasBacklight = res.getInteger(
                 com.android.internal.R.integer.config_buttonBrightnessSettingDefault) > 0;
 
