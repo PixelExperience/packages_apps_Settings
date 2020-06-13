eede7b2fa69b72bb0dfe3f589ee8cbe21f13855d
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-06-12 22:43:48 -0300

    StatusBarSettings: Cleanup and some fixes
    * Cleanup
    * Fix default value
    * Fix rtl behavior
    
    Change-Id: If56daebe497f715a21d30d681b846a4ece775f2e

--- src/com/android/settings/custom/statusbar/StatusBarSettings.java
+++ src/com/android/settings/custom/statusbar/StatusBarSettings.java
@@ -168,13 +168,13 @@ public class StatusBarSettings extends SettingsPreferenceFragment
         if (getResources().getConfiguration().getLayoutDirection() == View.LAYOUT_DIRECTION_RTL) {
             if (disallowCenteredClock) {
                 mStatusBarClock.setEntries(R.array.status_bar_clock_position_entries_notch_rtl);
-                mStatusBarClock.setEntryValues(R.array.status_bar_clock_position_values_notch_rtl);
+                mStatusBarClock.setEntryValues(R.array.status_bar_clock_position_values_notch);
             } else {
                 mStatusBarClock.setEntries(R.array.status_bar_clock_position_entries_rtl);
-                mStatusBarClock.setEntryValues(R.array.status_bar_clock_position_values_rtl);
+                mStatusBarClock.setEntryValues(R.array.status_bar_clock_position_values);
             }
             mQuickPulldown.setEntries(R.array.status_bar_quick_qs_pulldown_entries_rtl);
-            mQuickPulldown.setEntryValues(R.array.status_bar_quick_qs_pulldown_values_rtl);
+            mQuickPulldown.setEntryValues(R.array.status_bar_quick_qs_pulldown_values);
         } else if (disallowCenteredClock) {
             mStatusBarClock.setEntries(R.array.status_bar_clock_position_entries_notch);
             mStatusBarClock.setEntryValues(R.array.status_bar_clock_position_values_notch);
@@ -213,6 +213,13 @@ public class StatusBarSettings extends SettingsPreferenceFragment
 
     private void updateQuickPulldownSummary(int value) {
         String summary="";
+        if (getResources().getConfiguration().getLayoutDirection() == View.LAYOUT_DIRECTION_RTL){
+            if (value == PULLDOWN_DIR_LEFT) {
+                value = PULLDOWN_DIR_RIGHT;
+            }else if (value == PULLDOWN_DIR_RIGHT) {
+                value = PULLDOWN_DIR_LEFT;
+            }
+        }
         switch (value) {
             case PULLDOWN_DIR_NONE:
                 summary = getResources().getString(
@@ -229,11 +236,6 @@ public class StatusBarSettings extends SettingsPreferenceFragment
                 break;
         }
         mQuickPulldown.setSummary(summary);
-    }
-
-    private int getClockPosition() {
-        return Settings.System.getInt(getActivity().getContentResolver(),
-                STATUS_BAR_CLOCK_STYLE, 2);
     }
 
     @Override
