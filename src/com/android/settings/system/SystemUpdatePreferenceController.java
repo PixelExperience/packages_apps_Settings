7086824aae50a217142a3d3ffa2dafcc54736ab9
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-08-04 15:15:30 -0300

    Show updates also on CI build type
    Change-Id: I2ef49296342e545e46e98053f3d6820e0f7dc03c

--- src/com/android/settings/system/SystemUpdatePreferenceController.java
+++ src/com/android/settings/system/SystemUpdatePreferenceController.java
@@ -47,7 +47,7 @@ public class SystemUpdatePreferenceController extends BasePreferenceController {
     @Override
     public int getAvailabilityStatus() {
         String buildtype = SystemProperties.get(OTA_BUILD_TYPE_PROP,"unofficial");
-        if (!mUm.isAdminUser() || !buildtype.equalsIgnoreCase("official")){
+        if (!mUm.isAdminUser() || (!buildtype.equalsIgnoreCase("official") && !buildtype.equalsIgnoreCase("ci"))){
             return UNSUPPORTED_ON_DEVICE;
         }
         try {
