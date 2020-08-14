ce4c6a59cc61c5b47fd69fa43afa15c5e36b5afe
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-08-14 09:57:47 -0300

    Settings: Hide screen off fod if fingerprint not enrolled
    Change-Id: Iaed079179c8023b2d5f3e23b2b38aa6cfc7ba026

--- src/com/android/settings/security/FodGesturePreferenceController.java
+++ src/com/android/settings/security/FodGesturePreferenceController.java
@@ -18,6 +18,7 @@ package com.android.settings.security;
 
 import android.content.Context;
 import com.android.settings.core.BasePreferenceController;
+import com.android.settings.Utils;
 
 public class FodGesturePreferenceController extends BasePreferenceController {
 
@@ -25,6 +26,7 @@ public class FodGesturePreferenceController extends BasePreferenceController {
 
     public FodGesturePreferenceController(Context context, String preferenceKey) {
         super(context, preferenceKey);
+        fpm = Utils.getFingerprintManagerOrNull(context);
     }
 
     public FodGesturePreferenceController(Context context) {
@@ -39,6 +41,9 @@ public class FodGesturePreferenceController extends BasePreferenceController {
         }
         if (!mContext.getResources().getBoolean(
             com.android.internal.R.bool.config_supportsInDisplayFingerprintGesture)){
+            return UNSUPPORTED_ON_DEVICE;
+        }
+        if (fpm != null && (!fpm.isHardwareDetected() || !fpm.hasEnrolledFingerprints())) {
             return UNSUPPORTED_ON_DEVICE;
         }
         return AVAILABLE;
