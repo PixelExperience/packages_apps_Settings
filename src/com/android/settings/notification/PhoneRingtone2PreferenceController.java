07caf654fbff1bdf5adad9a13e4f72fc5c8f95ac
jhenrique09 <jhenrique09.mcz@hotmail.com>
2021-03-05 14:01:45 -0300

    Settings: Use AOSP translations for MSIM ringtone
    Change-Id: I048a161e33eaabe8d2eb46fee49cae844836258f

--- src/com/android/settings/notification/PhoneRingtone2PreferenceController.java
+++ src/com/android/settings/notification/PhoneRingtone2PreferenceController.java
@@ -41,6 +41,8 @@ public class PhoneRingtone2PreferenceController extends RingtonePreferenceContro
         DefaultRingtonePreference ringtonePreference =
                 (DefaultRingtonePreference) screen.findPreference(KEY_PHONE_RINGTONE2);
         ringtonePreference.setSlotId(SLOT_ID);
+        ringtonePreference.setTitle(mContext.getString(R.string.ringtone_title) + " - " +
+            String.format(mContext.getString(R.string.sim_card_number_title), 2));
         ringtonePreference.setEnabled(hasCard());
     }
 
